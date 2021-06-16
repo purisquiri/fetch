0b5c9761300977501987f7447ab5856947ef4b92
purisquiri <mau.dorigo@outlook.es>
2021-06-16 11:12:10 -0400

    fetch using Suspense component in parent component

---
+++ src/components/Users.vue
@@ -0,0 +1,22 @@
+<template>
+  <div v-for="user in users.data" :key="user.id">
+    {{ user.first_name }} {{ user.last_name }}
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+export default {
+  name: "Users",
+
+  async setup() {
+    const users = ref(null);
+    const userResponse = await fetch("https://reqres.in/api/users?delay=2");
+    users.value = await userResponse.json();
+
+    return { users };
+  },
+};
+</script>
+
+<style></style>
