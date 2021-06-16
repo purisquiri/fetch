63ffe4d78c2b5d72096c84d92bea975d4c45818e
purisquiri <mau.dorigo@outlook.es>
2021-06-16 11:41:12 -0400

    handling errors boundaries

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <Suspense>
-    <Users />
-  </Suspense>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <div v-if="error">Parent: {{ error }}</div>
+    <Suspense>
+      <template #default>
+        <Users />
+      </template>
+      <template #fallback>
+        <div>Loading users...</div>
+      </template>
+    </Suspense>
+  </div>
 </template>
 
 <script>
 import Users from "./components/Users.vue";
+import { ref, onErrorCaptured } from "vue";
 
 export default {
   name: "App",
+  setup() {
+    const error = ref(null);
+
+    onErrorCaptured((e) => {
+      error.value = e;
+    });
+
+    return { error };
+  },
   components: {
     Users,
   },
 };
 </script>
 
-<style></style>
+<style>
+#app {
+  text-align: center;
+}
+</style>
--- src/components/Users.vue
+++ src/components/Users.vue
@@ -1,5 +1,6 @@
 <template>
-  <div v-for="user in users.data" :key="user.id">
+  <div v-if="error">{{ error }}</div>
+  <div v-else v-for="user in users.data" :key="user.id">
     {{ user.first_name }} {{ user.last_name }}
   </div>
 </template>
@@ -10,13 +11,17 @@ export default {
   name: "Users",
 
   async setup() {
+    const error = ref(null);
     const users = ref(null);
-    const userResponse = await fetch("https://reqres.in/api/users?delay=2");
-    users.value = await userResponse.json();
 
-    return { users };
+    try {
+      const userResponse = await fetch("https://reqres.in/api/users?delay=2");
+      users.value = await userResponse.json();
+    } catch (e) {
+      error.value = e;
+    }
+
+    return { users, error };
   },
 };
 </script>
-
-<style></style>
